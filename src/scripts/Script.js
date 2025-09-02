070f2add80e90ab2191a384ace24858c70d76e30
Jiujiu Thong <jiujiu281999@gmail.com>
2025-09-02 21:33:43 +0700

    change className to classList

--- src/scripts/Script.js
+++ src/scripts/Script.js
@@ -1,4 +1,6 @@
 // add active class to the current nav-links (highlight it)
+// use className
+/*
 
 let header = document.getElementById("myNav");
 let btns = header.getElementsByClassName("nav-links-btn");
@@ -7,5 +9,21 @@ for (let i = 0; i < btns.length; i++) {
     let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+  });
+}
+
+*/
+
+// use classList
+
+let header = document.getElementById("myNav");
+let btns = header.getElementsByClassName("nav-links-btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].classList.remove("active");
+    }
+    this.classList.add("active");
   });
 }
